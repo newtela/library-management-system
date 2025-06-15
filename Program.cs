65f44409f8d0e5e71e69665ed8832d7fba029838
newtela <16074620+newtela@users.noreply.github.com>
2025-06-15 11:18:30 -0400

    bump

--- Program.cs
+++ Program.cs
@@ -3,7 +3,6 @@
 UserInterface userInterface = new();
 userInterface.MainMenu();
 
-
 // tuples example
 // (string Title, int Pages) book1 = ("Frankenstein", 350);
 // Console.WriteLine($"Title: {book1.Title}, Pages: {book1.Pages}");
