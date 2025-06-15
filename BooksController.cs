b9ed605117b7966bbb3c45c5f81207792d37311c
newtela <nqui@users.noreply.github.com>
2025-06-15 10:18:08 -0400

    use abstract base class for library items

--- BooksController.cs
+++ BooksController.cs
@@ -7,13 +7,31 @@ internal class BooksController
 {
    internal void ViewBooks()
    {
-      AnsiConsole.MarkupLine("[yellow]List of books:[/]");
+      var table = new Table();
+      table.Border(TableBorder.Rounded);
 
-      foreach (var book in MockDatabase.Books)
+      table.AddColumn("[yellow]ID[/]");
+      table.AddColumn("[yellow]Title[/]");
+      table.AddColumn("[yellow]Author[/]");
+      table.AddColumn("[yellow]Category[/]");
+      table.AddColumn("[yellow]Location[/]");
+      table.AddColumn("[yellow]Pages[/]");
+
+      var books = MockDatabase.LibraryItems.OfType<Book>();
+
+      foreach (var book in books)
       {
-         AnsiConsole.MarkupLine($"- [cyan]{book}[/]");
+         table.AddRow(
+            book.Id.ToString(),
+            $"[cyan]{book.Name}[/]",
+            $"[cyan]{book.Author}[/]",
+            $"[green]{book.Category}[/]",
+            $"[blue]{book.Location}[/]",
+            book.Pages.ToString()
+         );
       }
 
+      AnsiConsole.Write(table);
       AnsiConsole.MarkupLine("Press any key to continue");
       Console.ReadKey();
    }
@@ -21,24 +39,30 @@ internal class BooksController
    internal void AddBook()
    {
       var title = AnsiConsole.Ask<string>("Enter the [green]title[/] of the book to add:");
+      var author = AnsiConsole.Ask<string>("Enter the [green]author[/] of the book:");
+      var category = AnsiConsole.Ask<string>("Enter the [green]category[/] of the book:");
+      var location = AnsiConsole.Ask<string>("Enter the [green]location[/] of the book:");
+      var pages = AnsiConsole.Ask<int>("Enter the [green]number of pages[/] in the book:");
 
-      if (MockDatabase.Books.Contains(title))
+      if (MockDatabase.LibraryItems.OfType<Book>().Any(b => b.Name.Equals(title, StringComparison.OrdinalIgnoreCase)))
       {
          AnsiConsole.MarkupLine("[red]This book already exists[/]");
       }
       else
       {
-         MockDatabase.Books.Add(title);
+         var newBook = new Book(MockDatabase.LibraryItems.Count + 1, title, author, category, location, pages);
+         MockDatabase.LibraryItems.Add(newBook);
          AnsiConsole.MarkupLine("[green]Book added successfully[/]");
       }
-
       AnsiConsole.MarkupLine("Press any key to continue");
       Console.ReadKey();
    }
 
    internal void DeleteBook()
    {
-      if (MockDatabase.Books.Count == 0)
+
+      var books = MockDatabase.LibraryItems.OfType<Book>().ToList();
+      if (books.Count == 0)
       {
          AnsiConsole.MarkupLine("[red]No books available to delete");
          Console.ReadKey();
@@ -46,11 +70,13 @@ internal class BooksController
       }
 
       var bookToDelete = AnsiConsole.Prompt(
-         new SelectionPrompt<string>()
+         new SelectionPrompt<Book>()
             .Title("Select a [red]book[/] to delete:")
-            .AddChoices(MockDatabase.Books));
+            .UseConverter(b => $"{b.Name}")
+            .AddChoices(books)
+      );
 
-      if (MockDatabase.Books.Remove(bookToDelete))
+      if (MockDatabase.LibraryItems.Remove(bookToDelete))
       {
          AnsiConsole.MarkupLine("[red]Book deleted successfully[/]");
       }
---
+++ Models/Book.cs
@@ -0,0 +1,32 @@
+using Spectre.Console;
+using TCSA.OOP.LibraryManagementSystem.Models;
+
+namespace TCSA.OOP.LibraryManagementSystem;
+
+internal class Book : LibraryItem
+{
+    internal string Author { get; set; }
+    internal string Category { get; set; }
+    internal int Pages { get; set; }
+
+    internal Book(int id, string name, string author, string category, string location, int pages)
+        : base(id, name, location)
+    {
+        Author = author;
+        Category = category;
+        Pages = pages;
+    }
+
+    public override void DisplayDetails()
+    {
+        var panel = new Panel(new Markup($"[bold]Bool:[/] [cyan]{Name}[/] by [cyan]{Author}[/]") +
+                              $"\n[bold]Pages:[/] {Pages}" +
+                              $"\n[bold]Category:[/] [green]{Category}[/]" +
+                              $"\n[bold]Location:[/] [blue]{Location}[/]")
+        {
+            Border = BoxBorder.Rounded
+        };
+
+        AnsiConsole.Write(panel);
+    }
+}
